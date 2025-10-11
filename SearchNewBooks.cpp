c803c769d261bcc7483b0c826ce8600ed2989334
Munneth Gill <munnethgill@gmail.com>
2025-10-11 23:16:44 +0000

    Refactor Book class in SearchNewBooks.cpp to use ISBN and type, implement sorting functionality, and update readFile to return a vector of Book objects. Modify newbooks.dat to correct book types.

--- SearchNewBooks.cpp
+++ SearchNewBooks.cpp
@@ -3,39 +3,63 @@
 #include <fstream>
 #include <vector>
 #include <tuple>
+#include <algorithm>
 
 class Book{
     private:
-        int number;
+        int isbn;
+        std::string type;
         std::string language;
-        std::string format;
     public:
         //getter's
-        int getNumber(){return number;}
-        std::string getLanguage(){return language;}
-        std::string getFormat(){return format;}
+        int getISBN() const {return isbn;}
+        std::string getType() const {return type;}
+        std::string getLanguage() const {return language;}
 
         //constructor
-        Book(int number, std::string language, std::string format){
-            this->number = number;
+        Book(int isbn, std::string type, std::string language){
+            this->isbn = isbn;
+            this->type = type;
             this->language = language;
-            this->format = format;
         };
+
+        bool operator<(const Book& other) const {
+            if(this->isbn != other.isbn){
+                return this->isbn < other.isbn;
+            }
+            else if(this->type != other.type){
+                std::vector<std::string> typeOrder = {"new", "used", "digital"};
+                int thisIndex = -1, otherIndex = -1;
+                
+                for(int i = 0; i < typeOrder.size(); i++){
+                    if (this->type == typeOrder[i]) thisIndex = i;
+                    if (other.type == typeOrder[i]) otherIndex = i;
+                }
+                
+                return thisIndex < otherIndex;
+            }
+            else{
+                return this->language < other.language;
+            }
+        }
 };
 
-std::vector<std::tuple<int, std::string, std::string>> readFile(const std::string& file){
+/*
+* 2param
+*/
+std::vector<Book> readFile(const std::string& file){
     std::ifstream inputFile(file);
-    std::vector<std::tuple<int, std::string, std::string>> books;
+    std::vector<Book> books;
     std::string line;
     while (std::getline(inputFile, line)) {
         size_t comma1 = line.find(',');
         size_t comma2 = line.find(',', comma1 + 1);
         
-        int num = (int)line.substr(0, comma1);
-        std::string lang = line.substr(comma1 + 1, comma2 - comma1 - 1);
-        std::string format = line.substr(comma2 + 1);
+        int isbn = std::stoi(line.substr(0, comma1));
+        std::string type = line.substr(comma1 + 1, comma2 - comma1 - 1);
+        std::string language = line.substr(comma2 + 1);
         
-        books.push_back(std::make_tuple(num, lang, format));
+        books.push_back(Book(isbn, type, language));
         std::cout << line << std::endl;
     }
     return books;
@@ -47,12 +71,24 @@ std::string linSearch(std::vector<std::string>& books, std::string& request){
             return book;
         }
     }
+    return "Not found";
 }
 
-std::string binarySearch(std::vector<std::string>& books, std::string& request){
+int getTypePriority(const std::string& type) {
+    if (type == "new") return 0;
+    if (type == "used") return 1;
+    if (type == "digital") return 2;
+    return 3; // Default for any other type
+}
+
+void sortBooks(std::vector<Book>& books){
+    std::sort(books.begin(), books.end());
+}
+
+//std::string binarySearch(std::vector<std::string>& books, std::string& request){
     //sort books
     
-}
+//}
 
 char getSearchMethod() {
     std::string input;
@@ -78,11 +114,16 @@ char getSearchMethod() {
 int main(int argc, char *argv[]){
     std::string newbooks = argv[1];
     std::string request = argv[2];
-    std::vector<std::tuple<int, std::string, std::string>> newBooksVector = readFile(newbooks);
-    std::vector<std::tuple<int, std::string, std::string>> requestVector = readFile(request);
+    std::vector<Book> newBooksVector = readFile(newbooks);
+    std::vector<Book> requestVector = readFile(request);
     
-    char searchMethod = getSearchMethod();
-    std::cout << "Selected search method: " << searchMethod << std::endl;
+    //char searchMethod = getSearchMethod();
+    //std::cout << "Selected search method: " << searchMethod << std::endl;
+
+    sortBooks(newBooksVector);
+    for (const auto& book : newBooksVector){
+        std::cout << book.getISBN() << " " << book.getType() << " " << book.getLanguage() << std::endl;
+    }
     
     return 0;
 }
